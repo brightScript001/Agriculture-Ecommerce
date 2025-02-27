aa21fb8fc0c484eb7ee8c0ab30c80d57e3dd8887
Success Bright <bs3webdevelop@gmail.com>
2025-02-27 23:50:03 +0100

    push

--- index.js
+++ index.js
@@ -7,12 +7,54 @@
 // the new inhabitants come to live in the town is 50
 // the target population is p = 1200
 
-function yearToReactPopulation(p0, percent, aug, p) {
-  let years = 0;
-  while (p0 < p) {
-    p0 += (p0 * percent) / 100 + aug;
-    years++;
-  }
-  return years;
+// function yearToReactPopulation(p0, percent, aug, p) {
+//   let years = 0;
+//   while (p0 < p) {
+//     p0 += (p0 * percent) / 100 + aug;
+//     years++;
+//   }
+//   return years;
+// }
+// console.log(yearsToReachPopulation(1000, 2, 50, 1200));
+
+function reverseString(str) {
+  // to be able to reverse the string we have to use (.reverse) method and that is an array method, so we have to turn this string to an array.
+  return str.split("").reverse("").join("");
 }
-console.log(yearsToReachPopulation(1000, 2, 50, 1200));
+console.log(reverseString("Hey there!"));
+
+function MaxNum() {
+  let input = [4, 7, 1, 9, 3, 18];
+  let maxNum = input[0];
+  let i = 1;
+  while (i < input.length) {
+    if (input[i] >= maxNum) {
+      maxNum = input[i];
+    }
+    i++;
+  }
+  return maxNum;
+}
+console.log(MaxNum());
+
+// this function return a boolean value, if the string is a palindrome it will return true, otherwise it will return false.
+function CheckIfPalindrome(str) {
+  let reversedStr = str.split("").reverse().join("");
+  return str === reversedStr;
+}
+console.log(CheckIfPalindrome("madam"));
+
+function findTheVowels(str) {
+  let count = 0;
+  let vowels = ["a", "e", "i", "o", "u"];
+  let i = 0;
+  str = str.toLowerCase();
+  while (i < str.length) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+    i++;
+  }
+  return count;
+}
+console.log(findTheVowels("Hey there!"));
