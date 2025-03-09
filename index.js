2c1c3c6412fe0d5f01de73095871fcc8d4591291
Success Bright <bs3webdevelop@gmail.com>
2025-03-09 20:37:15 +0100

    feat(index.js): add removeDuplicates function to filter unique numbers from an array

--- index.js
+++ index.js
@@ -88,3 +88,14 @@ function findMax(arr) {
 function reverseStr(str) {
   return str.split("").reverse().join("");
 }
+
+function removeDuplicates(nums) {
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
+}
