633683bfe59269655e60439e600756d608887376
Success Bright <bs3webdevelop@gmail.com>
2025-03-28 21:13:40 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // function yearToReactPopulation(p0, percent, aug, p) {
-//   let years = 0;
+//   let years = 0
 //   while (p0 < p) {
 //     p0 += (p0 * percent) / 100 + aug;
 //   return years;
